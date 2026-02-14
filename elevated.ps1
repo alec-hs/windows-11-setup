e81953a96f195710be0683d003d8f5ef253bd53e
alec-hs <6503411+alec-hs@users.noreply.github.com>
2026-02-14 17:15:08 +0000

    Remove Update-WSL2 call and add missing user-functions import

--- elevated.ps1
+++ elevated.ps1
@@ -1,15 +1,17 @@
 Import-Module ".\modules\computer-functions.psm1"
 Import-Module ".\modules\core-functions.psm1"
 Import-Module ".\modules\app-functions.psm1"
+Import-Module ".\modules\user-functions.psm1"
 
 # Set Networks to Private
 Set-NetworkTypes
 
-# Enable Windows Features
-#Enable-HyperV
+# Power: High performance plan and enable hibernation
+Set-PowerPlanHighPerformance
+Enable-Hibernation
 
 # Install WSL2 with Debian
-#Install-WSL2
+Install-WSL2
 
 # Delete Desktop Shortcuts
 Remove-DesktopShortcuts
